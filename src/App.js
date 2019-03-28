92bec0a76358839cbda3844dc6591496de4e3cb0
Sonia Gorla <soniagorla@msn.com>
2019-03-27 22:27:23 -0500

    Create separate component for footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./Components/Footer.js";
 
 class App extends Component {
   render() {
@@ -20,42 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
-        <div className="footer__container">
-          <div className="footer__top">
-            <a href="#home" className="footer__top__link">
-              Home
-            </a>
-            <a href="#about" className="footer__top__link">
-              About
-            </a>
-            <a href="#success" className="footer__top__link">
-              Success
-            </a>
-            <a href="#story" className="footer__top__link">
-              Story
-            </a>
-            <a href="#volunteer" className="footer__top__link">
-              Volunteer
-            </a>
-            <a href="#donate" className="footer__top__link">
-              Donate
-            </a>
-            <a href="#photos" className="footer__top__link">
-              Photos
-            </a>
-            <a href="#contact" className="footer__top__link">
-              Contact
-            </a>
-          </div>
-          <div className="footer__bottom">
-            <div className="footer__bottom__company-name">
-              Latino Community Betterment Corportation
-            </div>
-            <div className="footer__bottom__copyright">
-              © 2019 | All Rights Reserved
-            </div>
-          </div>
-        </div>
+        <Footer />
       </div>
     );
   }
