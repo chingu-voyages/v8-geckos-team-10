9b7b7c0d3a0ae1e1a39a02c32f3069b3dbde0d69
shigra <sebastiangomezduranona@gmail.com>
2019-03-29 20:36:11 -0500

    add navbar component and styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./stylesheets/App.scss";
 import LandingPage from "./Components/LandingPage.js"
 import Footer from "./Components/Footer.js";
+import NavBar from "./Components/NavBar.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <LandingPage />
         <Footer />
       </div>
