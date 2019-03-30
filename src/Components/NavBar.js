48cd3978fad450cd59b6d5f5c3f03c6796c33e31
Sonia Gorla <soniagorla@msn.com>
2019-03-29 22:56:37 -0500

    Change footer size, make color variables

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,51 +2,51 @@ import React from "react";
 
 const NavBar = () => {
   return (
-      <div>
-        <ul>
-            <li>
-            <div className="btn-container">
-                <a href="#">Home</a>
-            </div>
-            </li>
+    <div>
+      <ul>
+        <li>
+          <div className="btn-container">
+            <a href="#">Home</a>
+          </div>
+        </li>
 
-            <li>
-            <div className="btn-container">
-                <a href="#">About Us</a>
-            </div>
-            </li>
+        <li>
+          <div className="btn-container">
+            <a href="#">About Us</a>
+          </div>
+        </li>
 
-            <li>
-            <div className="btn-container">
-                <a href="#">Diamond Stories</a>
-                </div>
-            </li>
+        <li>
+          <div className="btn-container">
+            <a href="#">Diamond Stories</a>
+          </div>
+        </li>
 
-            <li>
-            <div className="btn-container">
-                <a href="#">Photos</a>
-            </div>
-            </li>
+        <li>
+          <div className="btn-container">
+            <a href="#">Photos</a>
+          </div>
+        </li>
 
-            <li>
-            <div className="btn-container">
-                <a href="#">Volunteer</a>
-            </div>
-            </li>
+        <li>
+          <div className="btn-container">
+            <a href="#">Volunteer</a>
+          </div>
+        </li>
 
-            <li>
-            <div className="btn-container">
-                <a href="#">Donate</a>
-            </div>
-            </li>
+        <li>
+          <div className="btn-container">
+            <a href="#">Donate</a>
+          </div>
+        </li>
 
-            <li>
-            <div className="btn-container">
-                <a href="#">Contact Us</a>
-            </div>
-            </li>
-    </ul>
-</div>
+        <li>
+          <div className="btn-container">
+            <a href="#">Contact Us</a>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
 
