4408cb67a566b978cadf826dcea3b00d81a3b61f
Jeremy Huppe <jerhup@extendhealth.com>
2019-04-04 16:23:30 -0600

    Created skeleton of About component

---
+++ src/Components/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>About Us</h1>
+      <p>Latino CBC is a non-profit organization that works with the Douglas County Young Adult Court. 
+         YAC is a judicially supervised program that provides sentencing alternatives for youthful offenders 
+         up to age 25 who are charged with a felony to participate in a program of selective assessment and 
+         rehabilitative services administered by multidisciplinary agencies, Judge Shelly R. Stratman presides 
+         over the program. The information was provided by YAC Program in the Douglas County.
+
+         In the area of education Latino CBC encourages our YAC participants to get their GED or High School Diploma. 
+         Also, we help them with the process of enrollment and scholarship applications process for Community College 
+         and Universities in our Metropolitan area.
+
+​         In the area of employment, Latino CBC has a partnership with Goodwill Industries, Inc. together we create 
+         employment opportunities for our YAC participants assuring they obtain better jobs with great benefits. 
+         We provide the skills to the young adults to obtain and maintain employment, some of the skills needed accountability, 
+         dependability, and a positive outlook. With those skills that the young adults obtain through their employment search can 
+         be applied towards their personal lives such as being responsible for upcoming court hearings.
+
+         Latino CBC main focus is making a difference in other people’s lives and in our community that’s why Latino CBC believes 
+         it is very important to give back to our community in any way we can. This is the reason why Latino CBC believes and 
+         connects our YAC participants with other citizens who are in need, and other non -profits agencies for completion of their 
+         required community service.  
+
+         We believe everybody deserves  a second chance!</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
