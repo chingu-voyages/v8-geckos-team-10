3de35fef2de3f406abfbcb46d19b0dbc397ae8a0
Jeremy Huppe <jerhup@extendhealth.com>
2019-04-05 16:03:31 -0600

    Added bio section and updated styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,7 @@ const About = () => {
   return (
     <div className="about-us__container">
       <div className="organization__container">
-        <div className="organization__details">
-          <h1>About Us</h1>
+          <h1 className="about-title">ABOUT US</h1>
           <p>Latino CBC is a non-profit organization that works with the Douglas County Young Adult Court. 
             YAC is a judicially supervised program that provides sentencing alternatives for youthful offenders 
             up to age 25 who are charged with a felony to participate in a program of selective assessment and 
@@ -33,24 +32,142 @@ const About = () => {
           <p>
             We believe everybody deserves  a second chance!
           </p>
+      </div>
+      <h2>Meet the team</h2>
+      <div className="volunteer__bios">
+        <div className="individual__bio">
+          <img src="../assets/blancaMejia.jpg"></img>
+          <h3>Blanca Mejia</h3>
+          <p>
+          Blanca Mejia is the founder and Executive Director of Latino Community Betterment Corporation (Latino CBC) She has a bachelor 
+          law degree from UAG in Mexico. She also has a Communication Bachelor Degree from Bellevue University. She worked as a legal 
+          assistant for seven years at Ramirez Law Firm. Blanca is a very active community member. For more than five years, she has 
+          participated as part of the One World Clinic Board directors. Blanca has been married for 25 years and has three children. 
+          She is from Torreon, Coahuila, Mexico. In her free time, she enjoys exercising at Kroc Center, reading and spend time with her family.
+          </p>       
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/jamesProuty.jpg"></img>
+          <h3>James J. Prouty</h3>
+          <p>
+          James J. Prouty is the President of Latino Community Betterment Corporation. Latino CBC is an Non-Profit Organization serving the 
+          Omaha Latino Community. James is an Ex-President of Wessco Graphics a family business in Omaha establish in 1905. James is a former 
+          graduate from Dana College. He is a current President of the Omaha Business Men Association. He is an Ex-International/President of 
+          Service Club Cosmopolitan International in Mexico, United States, and Canada who’s focus is on raising money for Diabetes Research. 
+          He is a veteran of the 101 Airborne of the United States Army. James is a life-long resident of Omaha. Also, James has been married for 
+          48 years to his lovely wife Carol. James is currently active at the Salvation Army Kroc Center. 
+          </p>
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/erosMejia.jpg"></img>
+          <h3>Eros Mejia Hernandez</h3>
+          <p>
+          Eros Mejia-Hernandez is the current Vice President of the Latino Community Betterment Corporation. He is currently involved at 
+          Fraser Stryker PC LLO law firm. Eros has gotten 3 scholarships: The Buffet scholarship, the Senator Ashford scholarship, and a 
+          scholarship from Fraser Stryker law firm including an internship. Eros has received the Creighton Prep James Schwee Award and 
+          the Mahatma Gandhi Diversity Award. He was also involved in a business fraternity called Beta Gamma Sigma International Honors 
+          Society for Collegiate School of Business. 
+          </p>
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/oscarOrtiz.jpg"></img>
+          <h3>Oscar Ortiz</h3>
+          <p>
+          ​Legal Assistant/Office Manager
+          At TLN LAW, LLC
+          <br />
+          Profession
+          2013-Present, Legal Assistant/Office Manager at TLN LAW, LLC.
+          <br />
+          Education
+          2013- BFA Bachelor’s in Fine Arts Bellevue University at Omaha
+          2007- United Way Leadership Class of 2007 at Omaha
+          1997- Associates in Accounting at Morelia, Mex.
+          <br />
+          Volunteer
+          2004-2005 Nebraska Aids Project Omaha NE
+          2006 Catholic Charities Omaha NE
+          <br />
+          Professional Affiliations
+          Board member, Latino CBC
+          </p>
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/jenniferJazynka.jpg"></img>
+          <h3>Jennifer Jazynka</h3>
+          <p>
+          Jennifer Jazynka has been a Branch Manager at Omaha Public Library for the past ten years and has been a public librarian 
+          for the past fifteen years.  She is passionate about community, connections, and unfettered access to educational and informational 
+          resources in order to keep learning and become the best version of ourselves.  She lives with her husband Aaron and two children.
+          </p>        
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/franciscoZuniga.jpg"></img>
+          <h3>Francisco Zuniga</h3>
+          <p>
+          Francisco is a Board of Directors. He does refrigeration service sub. Tech School graduated from West Valley. Graduated from Occupational 
+          center California. Partner in a cheese factory with Latin Products in Hebron, Nebraska.
+          </p>
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/juanGarcia.jpg"></img>
+          <h3>Juan Garcia</h3>
+          <p>
+          Juan Garcia is treasurer of our organization.  He graduated from Bellevue West High School in 2014.  Juan  is a banker at Wells  Fargo bank. 
+          He is a student at UNO. He is majoring in business. 
+          </p>
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/jimSwanson.jpg"></img>
+          <h3>Jim Swanson</h3>
+          <p>
+          "Swanny" was born in Omaha in 1963. He is a graduate of Creighton Prep. He also attended Creighton University, where, in 1986, he earned a Bachelor's 
+          degree in Psychology and Secondary Education, and, in 1998, a Master's degree in Secondary School Counseling. After his undergrad at Creighton, Jim worked 
+          for four years on the East Coast with the Jesuit Volunteer Corps. Since that time, Jim has worked at Creighton Prep from 1991 to the present as a counselor, 
+          teacher, coach and mentor. Jim has lived in Guatemala and Ireland during two one-year sabbaticals from Prep. Jim is married to Frances Smyth Swanson, originally 
+          from Dublin, Ireland. He and Frances have two children, Dermot, a student at Regis University in Denver and Aidan, a senior at Creighton Prep. Jim's passion for 
+          Latino culture stems from his time in Guatemala, as well as twelve separate summer service trips to the Dominican Republic through Creighton's Institute for Latin 
+          American Concern.
+          </p>
+        </div>
+        <div className="individual__bio">
+          <img src="../assets/danielMejia.jpg"></img>
+          <h3>Daniel Mejia</h3>
+          <p>
+          Daniel is the volunteer coordinator at Latino CBC, where he has been given the opportunity to serve others since May 2016. He is a Sophomore at Creighton Prep. 
+          Outside of school, he likes to stay involved in other activities such as an Architecture/Engineering Exploration program, being with his youth group, and competing 
+          in cross country.
+          </p>
         </div>
       </div>
-      <div className="program__details">
-        <h3>Services</h3>
-        <ul>
-          <li>Legal Aid</li>
-          <li>Employment Assistance</li>
-          <li>Community Services</li>
-          <li>ESL Classes</li>
-          <li>Spanish Classes</li>
-          <li>Scholarship Information</li>
-          <li>Food Pantry</li>
-          <li>Legal Services for Immigration and Worker's Compensation Cases</li>\
-        </ul>
-      </div>
-      <div className="program__details">
-        <h3>Hours of Operation</h3>
-         <p>Monday-Friday 9:00am - 4:00pm</p>
+      <div className="company__details">
+        <div className="program__details">
+          <h3>Services</h3>
+          <ul className="about-page__list">
+            <li>Legal Aid</li> 
+            <br />
+            <li>Employment Assistance</li>
+            <br />
+            <li>Community Services</li>
+            <br />
+            <li>ESL Classes</li>
+            <br />
+            <li>Spanish Classes</li>
+            <br />
+            <li>Scholarship Information</li>
+            <br />
+            <li>Food Pantry</li>
+            <br />
+            <li>Legal Services for Immigration 
+              <br />
+              and Worker's Compensation Cases</li>
+            <br />
+          </ul>
+        </div>
+        <div className="program__details operation-hours">
+          <h3>Hours of Operation</h3>
+          <p>Monday-Friday 9:00am - 4:00pm</p>
+        </div>
       </div>     
     </div>
   );
