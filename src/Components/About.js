642b697be013d6a77f237d328930c5da4d534a00
Jeremy Huppe <jerhup@extendhealth.com>
2019-04-05 10:07:39 -0600

    Set up content for About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,32 +2,56 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="App">
-      <NavBar />
-      <h1>About Us</h1>
-      <p>Latino CBC is a non-profit organization that works with the Douglas County Young Adult Court. 
-         YAC is a judicially supervised program that provides sentencing alternatives for youthful offenders 
-         up to age 25 who are charged with a felony to participate in a program of selective assessment and 
-         rehabilitative services administered by multidisciplinary agencies, Judge Shelly R. Stratman presides 
-         over the program. The information was provided by YAC Program in the Douglas County.
-
-         In the area of education Latino CBC encourages our YAC participants to get their GED or High School Diploma. 
-         Also, we help them with the process of enrollment and scholarship applications process for Community College 
-         and Universities in our Metropolitan area.
-
-​         In the area of employment, Latino CBC has a partnership with Goodwill Industries, Inc. together we create 
-         employment opportunities for our YAC participants assuring they obtain better jobs with great benefits. 
-         We provide the skills to the young adults to obtain and maintain employment, some of the skills needed accountability, 
-         dependability, and a positive outlook. With those skills that the young adults obtain through their employment search can 
-         be applied towards their personal lives such as being responsible for upcoming court hearings.
-
-         Latino CBC main focus is making a difference in other people’s lives and in our community that’s why Latino CBC believes 
-         it is very important to give back to our community in any way we can. This is the reason why Latino CBC believes and 
-         connects our YAC participants with other citizens who are in need, and other non -profits agencies for completion of their 
-         required community service.  
-
-         We believe everybody deserves  a second chance!</p>
-      <Footer />
+    <div className="about-us__container">
+      <div className="organization__container">
+        <div className="organization__details">
+          <h1>About Us</h1>
+          <p>Latino CBC is a non-profit organization that works with the Douglas County Young Adult Court. 
+            YAC is a judicially supervised program that provides sentencing alternatives for youthful offenders 
+            up to age 25 who are charged with a felony to participate in a program of selective assessment and 
+            rehabilitative services administered by multidisciplinary agencies, Judge Shelly R. Stratman presides 
+            over the program. The information was provided by YAC Program in the Douglas County.
+          </p>
+          <p>
+            In the area of education Latino CBC encourages our YAC participants to get their GED or High School Diploma. 
+            Also, we help them with the process of enrollment and scholarship applications process for Community College 
+            and Universities in our Metropolitan area.
+          </p>
+          <p>
+  ​           In the area of employment, Latino CBC has a partnership with Goodwill Industries, Inc. Together we create 
+            employment opportunities for our YAC participants assuring they obtain better jobs with great benefits. 
+            We provide the skills to the young adults to obtain and maintain employment, accountability, 
+            dependability, and a positive outlook. These skills can be applied towards their personal lives such as 
+            being responsible for upcoming court hearings.
+          </p>
+          <p>
+            Latino CBC main focus is making a difference in other people’s lives and in our community that’s why Latino CBC believes 
+            it is very important to give back to our community in any way we can. This is the reason why Latino CBC believes and 
+            connects our YAC participants with other citizens who are in need, and other non -profits agencies for completion of their 
+            required community service.  
+          </p>
+          <p>
+            We believe everybody deserves  a second chance!
+          </p>
+        </div>
+      </div>
+      <div className="program__details">
+        <h3>Services</h3>
+        <ul>
+          <li>Legal Aid</li>
+          <li>Employment Assistance</li>
+          <li>Community Services</li>
+          <li>ESL Classes</li>
+          <li>Spanish Classes</li>
+          <li>Scholarship Information</li>
+          <li>Food Pantry</li>
+          <li>Legal Services for Immigration and Worker's Compensation Cases</li>\
+        </ul>
+      </div>
+      <div className="program__details">
+        <h3>Hours of Operation</h3>
+         <p>Monday-Friday 9:00am - 4:00pm</p>
+      </div>     
     </div>
   );
 };
