7c73493510e430962f165558a82945e65fd9ffa5
Sonia Gorla <soniagorla@msn.com>
2019-04-05 00:12:06 -0500

    Fix positioning issues, change font style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./stylesheets/App.scss";
-import LandingPage from "./Components/LandingPage.js"
+import LandingPage from "./Components/LandingPage.js";
 import Footer from "./Components/Footer.js";
-import NavBar from "./Components/NavBar.js"
+import NavBar from "./Components/NavBar.js";
+import Donate from "./Components/Donate.js";
 
 class App extends Component {
   render() {
