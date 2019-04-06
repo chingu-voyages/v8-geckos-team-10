a29f0a379e7db59e2005309ccfaa9be9013e2376
shigra <sebastiangomezduranona@gmail.com>
2019-04-06 14:37:46 -0500

    refactor navbar classes to avoid future conflicts

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,50 +3,51 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="navBar">
-      <ul className="navBar-list">
-        <li>
-          <div className="btn-container">
-            <a href="#home">Home</a>
-          </div>
-        </li>
+        <ul className="navBar-list">
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#home">Home</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="btn-container">
-            <a href="#about">About Us</a>
-          </div>
-        </li>
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#about">About Us</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="btn-container">
-            <a href="#story">Diamond Stories</a>
-          </div>
-        </li>
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#story">Diamond Stories</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="btn-container">
-            <a href="#photos">Photos</a>
-          </div>
-        </li>
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#photos">Photos</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="btn-container">
-            <a href="#volunteer">Volunteer</a>
-          </div>
-        </li>
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#volunteer">Volunteer</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="btn-container">
-            <a href="#donate">Donate</a>
-          </div>
-        </li>
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#donate">Donate</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="btn-container">
-            <a href="#contact">Contact Us</a>
-          </div>
-        </li>
-      </ul>
-    </div>
+          <li className='navElement'>
+            <div className="btn-container">
+              <a className='navLink' href="#contact">Contact Us</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      
   );
 };
 
