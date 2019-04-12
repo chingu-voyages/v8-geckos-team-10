9bcc91897b5482c53431befed7c6dd74b9efd064
SebasDuranona <sebastiangomezduranona@gmail.com>
2019-04-12 15:55:59 -0500

    add basic image slider (needs further work)

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -5,7 +5,23 @@ const Photos = () => {
   return (
       <div className='photos_container'>
         <h1>PHOTO GALLERY</h1>
-        
+        <div className='slide-container'>
+        	<span id='slider-image-1'></span>
+        	<span id='slider-image-2'></span>
+        	<span id='slider-image-3'></span>
+
+        	<div className='image-container'>
+        		<img src='https://images.unsplash.com/photo-1463946377180-f5185c2783e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' className='slider-image' alt='image 1'/>
+        		<img src='https://images.unsplash.com/photo-1463946377180-f5185c2783e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' className='slider-image' alt='image 2'/>
+        		<img src='https://images.unsplash.com/photo-1463946377180-f5185c2783e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' className='slider-image' alt='image 3'/>
+        	</div>
+        	<div className='button-container'>
+        		<a href='#slider-image-1' className='slider-button' alt='image 1'> </a>
+        		<a href='#slider-image-2' className='slider-button' alt='image 2'> </a>
+        		<a href='#slider-image-3' className='slider-button' alt='image 3'> </a>
+
+        	</div>
+        </div>
       </div>
   );
 };
