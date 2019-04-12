f2862136f94fbfcdc6c20bef76d03fdcb061266d
shigra <sebastiangomezduranona@gmail.com>
2019-04-11 21:02:21 -0500

    add phottos component and stylesheet

---
+++ src/Components/Photos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../stylesheets/Photos.scss";
+
+const Photos = () => {
+  return (
+      <div>
+          <h1>Soon to Be Photos Page!</h1>
+      </div>
+  );
+};
+
+export default Photos;
