02685e7a80f5cb812d195600b339dc5801999bb4
Gomez-Duranona <sgomezduranona@mail.mccneb.edu>
2019-04-19 14:13:26 -0500

    add alt tags to images

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,31 +1,30 @@
 import React from "react";
 import "../stylesheets/Photos.scss";
 
+
 const Photos = () => {
   return (
   <div className="photos_container">
       <h1 className="photos_title">PHOTO GALLERY</h1>
-      <div class="row"> 
-        <div class="column">
-          {/* <img src="../assets/photos_page/photo1.jpg"/> */}
-          <img src="../assets/photos_page/photo2.jpg"/>
-          <img src="../assets/photos_page/photo3.jpg"/>
+      <div className="row"> 
+        <div className="column">
+          <img className='image'src="../assets/photos_page/photo2.jpg" alt='food pantry service'/>
+          <img className='image' src="../assets/photos_page/photo3.jpg" alt='community service'/>
           
         </div>
-        <div class="column">
-          <img src="../assets/photos_page/photo4.jpg"/>
-          <img src="../assets/photos_page/photo5.jpg"/>
-          <img src="../assets/photos_page/photo6.jpg"/>
+        <div className="column">
+          <img className='image' src="../assets/photos_page/photo4.jpg" alt='young man in his graduation after completing the program'/>
+          <img className='image' src="../assets/photos_page/photo5.jpg" alt='Employment and financial counseling'/>
+          <img className='image' src="../assets/photos_page/photo6.jpg" alt='English as a Second Language class'/>
         </div> 
-        <div class="column">
-          <img src="../assets/photos_page/photo7.jpg"/>
-          <img src="../assets/photos_page/photo8.png"/>
-          <img src="../assets/photos_page/photo9.jpg"/>
+        <div className="column">
+          <img className='image' src="../assets/photos_page/photo7.jpg" alt='One World Clinic Partership'/>
+          <img className='image' src="../assets/photos_page/photo8.png" alt='young man celebrating his return to college'/>
+          <img className='image' src="../assets/photos_page/photo9.jpg" alt='food pantry service'/>
         </div>
-        <div class="column">
-          <img src="../assets/photos_page/photo10.png"/>
-          <img src="../assets/photos_page/photo11.jpg"/>
-          {/* <img src="../assets/photos_page/photo1.jpg"/> */}
+        <div className="column">
+          <img className='image' src="../assets/photos_page/photo10.png" alt='thanksgiving lunch'/>
+          <img className='image' src="../assets/photos_page/photo11.jpg" alt='young man registering at Metropolitan Community College'/>
         </div>
       </div>
   </div>
