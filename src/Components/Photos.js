7691a544b605d3f1274cd958e4559e86aa247aa2
Gomez-Duranona <sgomezduranona@mail.mccneb.edu>
2019-04-19 11:15:55 -0500

    create responsive image grid in Photos page

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -3,43 +3,32 @@ import "../stylesheets/Photos.scss";
 
 const Photos = () => {
   return (
-    <div className="photos_container">
+  <div className="photos_container">
       <h1 className="photos_title">PHOTO GALLERY</h1>
-      <div className="slide-container">
-        <span id="slider-image-1" />
-        <span id="slider-image-2" />
-        <span id="slider-image-3" />
-
-        <div className="image-container">
-          <img
-            src="https://images.unsplash.com/photo-1463946377180-f5185c2783e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"
-            className="slider-image"
-            alt="1"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1463946377180-f5185c2783e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"
-            className="slider-image"
-            alt="2"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1463946377180-f5185c2783e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"
-            className="slider-image"
-            alt="3"
-          />
+      <div class="row"> 
+        <div class="column">
+          {/* <img src="../assets/photos_page/photo1.jpg"/> */}
+          <img src="../assets/photos_page/photo2.jpg"/>
+          <img src="../assets/photos_page/photo3.jpg"/>
+          
         </div>
-        <div className="button-container">
-          <a href="#slider-image-1" className="slider-button" alt="image 1">
-            {" "}
-          </a>
-          <a href="#slider-image-2" className="slider-button" alt="image 2">
-            {" "}
-          </a>
-          <a href="#slider-image-3" className="slider-button" alt="image 3">
-            {" "}
-          </a>
+        <div class="column">
+          <img src="../assets/photos_page/photo4.jpg"/>
+          <img src="../assets/photos_page/photo5.jpg"/>
+          <img src="../assets/photos_page/photo6.jpg"/>
+        </div> 
+        <div class="column">
+          <img src="../assets/photos_page/photo7.jpg"/>
+          <img src="../assets/photos_page/photo8.png"/>
+          <img src="../assets/photos_page/photo9.jpg"/>
+        </div>
+        <div class="column">
+          <img src="../assets/photos_page/photo10.png"/>
+          <img src="../assets/photos_page/photo11.jpg"/>
+          {/* <img src="../assets/photos_page/photo1.jpg"/> */}
         </div>
       </div>
-    </div>
+  </div>
   );
 };
 
